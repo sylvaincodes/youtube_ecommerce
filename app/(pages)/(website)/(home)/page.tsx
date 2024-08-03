3f2fa6815482dd97a94283c05b537c6ed45fae58
sylvaincodes <adandesylvain2010@gmail.com>
2024-08-03 21:12:44 +0200

    5- Config Typescript and Eslint

--- app/(pages)/(website)/(home)/page.tsx
+++ app/(pages)/(website)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import * as React from 'react'
 
 export default function Home() {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import * as React from 'react'
 
 const inter = Inter({ subsets: ["latin"] });
 
