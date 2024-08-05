2c298092b5c1a06050dc6e3698fd7ad493d16008
sylvaincodes <adandesylvain2010@gmail.com>
2024-08-05 02:02:28 +0200

    Resolve bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
 
-    experimental:{
-        forceSwcTransforms: true
-    }
-
+  distDir: "build",
 };
 
 export default nextConfig;
