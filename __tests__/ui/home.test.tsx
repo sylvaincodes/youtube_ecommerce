cd2e2fa39929a4f36e398d480fdea077b0b73a84
sylvaincodes <adandesylvain2010@gmail.com>
2024-08-05 16:00:21 +0200

    CI CD workflows update

--- __tests__/ui/home.test.tsx
+++ __tests__/ui/home.test.tsx
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { expect, test, describe } from "@jest/globals";
 import Page from "@/app/(pages)/(website)/(home)/page"; // need to access DOM
-import * as React from 'react'
+import * as React from "react";
 
 describe("Testing home UI: Unit testing", () => {
   test("display the homepage", () => {
